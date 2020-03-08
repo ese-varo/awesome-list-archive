a92d6f8dc84fe1d57a0642257d946e15b4087433
Alvaro <varoboss.ap@gmail.com>
2020-03-08 00:29:08 -0600

    Setup email functionality for sign-up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,4 +8,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require turbolinks
 //= require_tree .
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '578',
+  :authentication => :plain,
+  :user_name => ENV['SENGRID_USERNAME'],
+  :password => ENV['SENGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
